aa4aa4615805dff174b8831d0205a946a0263d49
Marvin Velásquez <immavlgt@gmail.com>
2023-04-13 09:23:44 -0600

    feat(Setup): script to bootstrap the cluster env (#2)
    * feat(Setup): script to bootstrap the cluster env
    
    * fix(Setup): script output
    
    * fix(Setup): set cluster profile
    
    * docs(Setup): add setup.sh docs
    
    * fix(Setup): clean up

---
+++ 01/setup.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+minikube profile challenge01
+mkdir csr/
+
+openssl genrsa -out $(pwd)/martin.key 4096
+
+cat <<STDIN > csr/martin.csr.cnf
+[ req ]
+default_bits = 2048
+prompt = no
+default_md = sha256 # algoritmo de encriptacion
+distinguished_name = dn
+[ dn ]
+CN = martin
+O = developers
+[ v3_ext ]
+authorityKeyIdentifier=keyid,issuer:always
+basicConstraints=CA:FALSE
+keyUsage=keyEncipherment,dataEncipherment
+extendedKeyUsage=serverAuth,clientAuth
+STDIN
+
+openssl req -config csr/martin.csr.cnf -new -key martin.key -nodes -out csr/martin.csr
+
+cat <<STDIN > martin-csr.yaml
+apiVersion: certificates.k8s.io/v1
+kind: CertificateSigningRequest
+metadata:
+  name: martin-cluster-authentication
+spec:
+  groups:
+    - system:authenticated
+  request: $(cat $(pwd)/csr/martin.csr | base64 | tr -d '\n')
+  signerName: kubernetes.io/kube-apiserver-client
+  usages:
+  - client auth
+STDIN
+
+kubectl apply -f martin-csr.yaml
+
+kubectl certificate approve martin-cluster-authentication
+
+kubectl get csr martin-cluster-authentication -o jsonpath='{.status.certificate}' | base64 --decode > martin.pem
+rm -rf csr martin-csr.yaml
+ls martin.key  martin.pem
